cb92d8fc3c518d753e2a29cc7807b27b050572fe
ThixVinix <tvasouza@hotmail.com>
2024-09-27 00:08:28 -0300

    Utilização de Record

--- src/main/java/com/example/exampleproject/configs/exceptions/ErrorResponse.java
+++ src/main/java/com/example/exampleproject/configs/exceptions/ErrorResponse.java
@@ -1,27 +1,17 @@
 package com.example.exampleproject.configs.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Builder
-@Setter
-@Getter
-public class ErrorResponse {
-    private LocalDateTime timestamp;
-    private int status;
-    private String error;
-    private String message;
-    private String path;
-
-    public ErrorResponse(LocalDateTime timestamp, int status, String error, String message, String path) {
-        this.timestamp = timestamp;
-        this.status = status;
-        this.error = error;
-        this.message = message;
-        this.path = path;
-    }
-
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public record ErrorResponse(
+        LocalDateTime timestamp,
+        int status,
+        String error,
+        String message,
+        String path
+) {
 }
