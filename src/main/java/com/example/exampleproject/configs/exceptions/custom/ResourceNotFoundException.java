cc1b1a3ca6b24e14b45830b94f274cb618d1118b
ThixVinix <tvasouza@hotmail.com>
2024-09-30 19:00:18 -0300

    Adicionado anotação @NoArgsConstructor.

--- src/main/java/com/example/exampleproject/configs/exceptions/custom/ResourceNotFoundException.java
+++ src/main/java/com/example/exampleproject/configs/exceptions/custom/ResourceNotFoundException.java
@@ -1,5 +1,8 @@
 package com.example.exampleproject.configs.exceptions.custom;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
