51e6005034826e536b9d7394eeab0e793c953e63
ThixVinix <tvasouza@hotmail.com>
2024-09-30 19:02:02 -0300

    Inclusão dos campos dataInicial, dataFinal, telefone, username, dataDeValidade e preço. Adiciona também validações e formatações necessárias para os novos campos.

--- src/main/java/com/example/exampleproject/dto/request/TestPostRequest.java
+++ src/main/java/com/example/exampleproject/dto/request/TestPostRequest.java
@@ -1,15 +1,26 @@
 package com.example.exampleproject.dto.request;
 
+import com.example.exampleproject.configs.annotations.ValidDateRange;
 import jakarta.validation.constraints.*;
 import lombok.Builder;
 import lombok.Getter;
 import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Builder
 @Getter
+@ValidDateRange(dateAField = "dataInicial", dateBField = "dataFinal")
 public class TestPostRequest {
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime dataInicial;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime dataFinal;
 
     @NotBlank(message = "{field.name.required}")
     private String nome;
@@ -34,5 +45,20 @@ public class TestPostRequest {
 
     @NotBlank(message = "{field.base64Image.required}")
     private String imagemBase64;
+    
+    @NotBlank(message = "{field.phoneNumber.required}")
+    @Pattern(regexp = "\\(\\d{2}\\) \\d{4,5}-\\d{4}", message = "{field.phoneNumber.invalidFormat}")
+    private String telefone;
+
+    @Size(min = 3, max = 30, message = "{field.username.size}")
+    private String username;
+
+    @Future(message = "{field.expiryDate.future}")
+    @NotNull(message = "{field.expiryDate.required}")
+    private LocalDate dataDeValidade;
+
+    @DecimalMin(value = "0.0", inclusive = false, message = "{field.price.min}")
+    @Digits(integer = 5, fraction = 2, message = "{field.price.invalidFormat}")
+    private BigDecimal preco;
     
 }
