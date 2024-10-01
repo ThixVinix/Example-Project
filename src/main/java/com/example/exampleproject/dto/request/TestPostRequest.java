d3032272e2a60f6e31c8167d7a9d80729a683c8b
ThixVinix <tvasouza@hotmail.com>
2024-10-01 20:41:05 -0300

    Substitui a classe TestPostRequest por um record e adicionei a anotação de JSON deserializers personalizados. Isso simplifica o código e melhora a legibilidade.

--- src/main/java/com/example/exampleproject/dto/request/TestPostRequest.java
+++ src/main/java/com/example/exampleproject/dto/request/TestPostRequest.java
@@ -1,64 +1,65 @@
 package com.example.exampleproject.dto.request;
 
 import com.example.exampleproject.configs.annotations.ValidDateRange;
+import com.example.exampleproject.configs.deserializers.CustomLocalDateDeserializer;
+import com.example.exampleproject.configs.deserializers.CustomLocalDateTimeDeserializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
 import jakarta.validation.constraints.*;
-import lombok.Builder;
-import lombok.Getter;
 import org.hibernate.validator.constraints.br.CPF;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Builder
-@Getter
 @ValidDateRange(dateAField = "dataInicial", dateBField = "dataFinal")
-public class TestPostRequest {
+public record TestPostRequest(
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime dataInicial;
+        @JsonDeserialize(using = CustomLocalDateTimeDeserializer.class)
+        LocalDateTime dataInicial,
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime dataFinal;
+        @JsonDeserialize(using = CustomLocalDateTimeDeserializer.class)
+        LocalDateTime dataFinal,
 
-    @NotBlank(message = "{field.name.required}")
-    private String nome;
+        @NotBlank(message = "{field.name.required}")
+        String nome,
 
-    @NotNull(message = "{field.birthdate.required}")
-    @Past(message = "{field.birthdate.past}")
-    private LocalDate dataNascimento;
+        @NotNull(message = "{field.birthdate.required}")
+        @Past(message = "{field.birthdate.past}")
+        @JsonDeserialize(using = CustomLocalDateDeserializer.class)
+        LocalDate dataNascimento,
 
-    @NotBlank(message = "{field.email.required}")
-    @Email(message = "{field.email.invalidFormat}")
-    private String email;
+        @NotBlank(message = "{field.email.required}")
+        @Email
+        String email,
 
-    @NotBlank(message = "{field.cpf.required}")
-    @CPF(message = "{field.cpf.invalid}")
-    @Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}", message = "{field.cpf.invalidFormat}")
-    private String cpf;
+        @NotBlank(message = "{field.cpf.required}")
+        @CPF(message = "{field.cpf.invalid}")
+        @Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}", message = "{field.cpf.invalidFormat}")
+        String cpf,
 
-    @NotNull(message = "{field.age.required}")
-    @Min(value = 0, message = "{field.age.minimum}")
-    @Max(value = Byte.MAX_VALUE, message = "{field.age.maximum}")
-    private Byte idade;
+        @NotNull(message = "{field.age.required}")
+        @Min(value = 0, message = "{field.age.minimum}")
+        @Max(value = Byte.MAX_VALUE, message = "{field.age.maximum}")
+        Byte idade,
 
-    @NotBlank(message = "{field.base64Image.required}")
-    private String imagemBase64;
-    
-    @NotBlank(message = "{field.phoneNumber.required}")
-    @Pattern(regexp = "\\(\\d{2}\\) \\d{4,5}-\\d{4}", message = "{field.phoneNumber.invalidFormat}")
-    private String telefone;
+        @NotBlank(message = "{field.base64Image.required}")
+        String imagemBase64,
 
-    @Size(min = 3, max = 30, message = "{field.username.size}")
-    private String username;
+        @NotBlank(message = "{field.phoneNumber.required}")
+        @Pattern(regexp = "\\(\\d{2}\\) \\d{4,5}-\\d{4}", message = "{field.phoneNumber.invalidFormat}")
+        String telefone,
 
-    @Future(message = "{field.expiryDate.future}")
-    @NotNull(message = "{field.expiryDate.required}")
-    private LocalDate dataDeValidade;
+        @Size(min = 3, max = 30, message = "{field.username.size}")
+        String username,
 
-    @DecimalMin(value = "0.0", inclusive = false, message = "{field.price.min}")
-    @Digits(integer = 5, fraction = 2, message = "{field.price.invalidFormat}")
-    private BigDecimal preco;
-    
+        @Future(message = "{field.expiryDate.future}")
+        @NotNull(message = "{field.expiryDate.required}")
+        @JsonDeserialize(using = CustomLocalDateDeserializer.class)
+        LocalDate dataDeValidade,
+
+        @DecimalMin(value = "0.0", inclusive = false, message = "{field.price.min}")
+        @Digits(integer = 5, fraction = 2, message = "{field.price.invalidFormat}")
+        BigDecimal preco
+) {
 }
