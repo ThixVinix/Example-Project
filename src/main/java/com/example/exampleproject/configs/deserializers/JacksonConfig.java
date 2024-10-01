fa6ed48fd7641729e6138704296bef018a2b146b
ThixVinix <tvasouza@hotmail.com>
2024-10-01 20:42:18 -0300

    Criada a classe JacksonConfig para configurar o ObjectMapper com JavaTimeModule registrado e desativar a serialização de datas como timestamps. Isso melhora o tratamento de datas em aplicações Spring.

---
+++ src/main/java/com/example/exampleproject/configs/deserializers/JacksonConfig.java
@@ -0,0 +1,25 @@
+package com.example.exampleproject.configs.deserializers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    /**
+     * Configures and provides a customized {@link ObjectMapper} bean.
+     *
+     * @return a configured {@link ObjectMapper} instance with JavaTimeModule registered
+     *         and serialization feature WRITE_DATES_AS_TIMESTAMPS disabled.
+     */
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
+    }
+}
