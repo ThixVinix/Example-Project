3dd013dd9b29bb1181a60e2d8a9975b5e31486f8
ThixVinix <tvasouza@hotmail.com>
2024-10-01 20:41:35 -0300

    Substitui os parâmetros de Locale pelos valores padrão de LocaleContextHolder. Isso simplifica as chamadas de métodos, eliminando a necessidade de passar explicitamente o Locale.

--- src/main/java/com/example/exampleproject/utils/messages/MessageUtils.java
+++ src/main/java/com/example/exampleproject/utils/messages/MessageUtils.java
@@ -2,6 +2,7 @@ package com.example.exampleproject.utils.messages;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PostConstruct;
@@ -77,26 +78,24 @@ public class MessageUtils {
     /**
      * Retrieves a localized message for the given key and locale.
      *
-     * @param key    the message key to look up, such as 'error.notfound'
-     * @param locale the locale in which to resolve the message
+     * @param key the message key to look up, such as 'error.notfound'
      * @return the resolved message as a String
      */
     @SuppressWarnings("unused")
-    public static String getMessage(String key, Locale locale) {
-        return messageSourceStatic.getMessage(key, null, locale);
+    public static String getMessage(String key) {
+        return messageSourceStatic.getMessage(key, null, LocaleContextHolder.getLocale());
     }
 
 
     /**
      * Retrieves a localized message for the given key and locale, with optional arguments.
      *
-     * @param key    the message key to look up, such as 'error.notfound'
-     * @param locale the locale in which to resolve the message
-     * @param args   an array of arguments that will be filled in for params within the message (optional)
+     * @param key  the message key to look up, such as 'error.notfound'
+     * @param args an array of arguments that will be filled in for params within the message (optional)
      * @return the resolved message as a String
      */
     @SuppressWarnings("unused")
-    public static String getMessage(String key, Locale locale, Object... args) {
-        return messageSourceStatic.getMessage(key, args, locale);
+    public static String getMessage(String key, Object... args) {
+        return messageSourceStatic.getMessage(key, args, LocaleContextHolder.getLocale());
     }
 }
