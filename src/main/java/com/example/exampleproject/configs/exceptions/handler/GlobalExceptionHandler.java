abd1969de2f4f8b917b6931de02e355cac182bdf
ThixVinix <tvasouza@hotmail.com>
2024-10-02 21:09:39 -0300

    Removido manipulador específico para MethodArgumentNotValidException e unificado com outros manipuladores de bad request. Alterada nomenclatura de "message" para "messages" em várias respostas de erro, e excluído código não utilizado para maior clareza.

--- src/main/java/com/example/exampleproject/configs/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/exampleproject/configs/exceptions/handler/GlobalExceptionHandler.java
@@ -1,17 +1,14 @@
 package com.example.exampleproject.configs.exceptions.handler;
 
-import com.example.exampleproject.configs.exceptions.ErrorMapResponse;
 import com.example.exampleproject.configs.exceptions.ErrorResponse;
 import com.example.exampleproject.configs.exceptions.custom.BusinessException;
 import com.example.exampleproject.configs.exceptions.custom.ResourceNotFoundException;
 import com.example.exampleproject.configs.exceptions.handler.helper.ExceptionHandlerHelper;
-import com.example.exampleproject.utils.messages.MessageUtils;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.FieldError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
@@ -22,8 +19,6 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.time.LocalDateTime;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Slf4j
 @ControllerAdvice
@@ -33,47 +28,16 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleResourceNotFoundException(Exception ex,
                                                                          WebRequest request) {
         log.error("Resource not found: {}", ex.getMessage(), ex);
+
         ErrorResponse errorResponse = ErrorResponse.builder()
                 .timestamp(LocalDateTime.now())
                 .status(HttpStatus.NOT_FOUND.value())
                 .error(HttpStatus.NOT_FOUND.getReasonPhrase())
-                .message(ExceptionHandlerHelper.getNotFoundMessage(ex))
+                .messages(ExceptionHandlerHelper.getNotFoundMessage(ex))
                 .path(request.getDescription(Boolean.FALSE))
                 .build();
 
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorMapResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex,
-                                                                                  WebRequest request) {
-        log.error("Method argument not valid: {}", ex.getMessage(), ex);
-        Map<String, String> errorMessages = ex.getBindingResult().getFieldErrors()
-                .stream()
-                .collect(Collectors.toMap(
-                        FieldError::getField,
-                        error -> error.getDefaultMessage() != null ?
-                                error.getDefaultMessage() : MessageUtils.getMessage("argument.type.invalid"),
-                        (existingValue, newValue) -> {
-                            if (existingValue.endsWith(".")) {
-                                existingValue =
-                                        existingValue.substring(0, existingValue.length() - 1) + "; " + newValue;
-                            } else {
-                                existingValue = existingValue + "; " + newValue;
-                            }
-                            return existingValue;
-                        }
-                ));
-
-        ErrorMapResponse errorResponse = ErrorMapResponse.builder()
-                .timestamp(LocalDateTime.now())
-                .status(HttpStatus.BAD_REQUEST.value())
-                .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
-                .messages(errorMessages)
-                .path(request.getDescription(Boolean.FALSE))
-                .build();
-
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
@@ -84,16 +48,15 @@ public class GlobalExceptionHandler {
                 .timestamp(LocalDateTime.now())
                 .status(HttpStatus.METHOD_NOT_ALLOWED.value())
                 .error(HttpStatus.METHOD_NOT_ALLOWED.getReasonPhrase())
-                .message(MessageUtils.getMessage(
-                        "http.method.not.supported",
-                        ex.getMethod()))
+                .messages(ExceptionHandlerHelper.getMethodNotAllowedMessage(ex))
                 .path(request.getDescription(Boolean.FALSE))
                 .build();
 
         return new ResponseEntity<>(errorResponse, HttpStatus.METHOD_NOT_ALLOWED);
     }
 
-    @ExceptionHandler({HttpMessageNotReadableException.class,
+    @ExceptionHandler({MethodArgumentNotValidException.class,
+            HttpMessageNotReadableException.class,
             MissingServletRequestParameterException.class,
             BusinessException.class,
             MethodArgumentTypeMismatchException.class,
@@ -104,7 +67,7 @@ public class GlobalExceptionHandler {
                 .timestamp(LocalDateTime.now())
                 .status(HttpStatus.BAD_REQUEST.value())
                 .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
-                .message(ExceptionHandlerHelper.getBadRequestMessage(ex))
+                .messages(ExceptionHandlerHelper.getBadRequestMessage(ex))
                 .path(request.getDescription(Boolean.FALSE))
                 .build();
 
@@ -119,8 +82,7 @@ public class GlobalExceptionHandler {
                 .timestamp(LocalDateTime.now())
                 .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
                 .error(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
-                .message(ex.getMessage() != null ?
-                        ex.getMessage() : MessageUtils.getMessage("unknown.exception.error"))
+                .messages(ExceptionHandlerHelper.getInternalServerErrorMessage(ex))
                 .path(request.getDescription(Boolean.FALSE))
                 .build();
 
