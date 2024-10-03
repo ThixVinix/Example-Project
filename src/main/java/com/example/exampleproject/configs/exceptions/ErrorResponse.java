07cd31052bc24a87b674de800027b3c9b11db666
ThixVinix <tvasouza@hotmail.com>
2024-10-02 21:11:35 -0300

    Substitui o campo 'message' por 'messages', que é um Map para suportar múltiplas mensagens de erro. Além disso, modifiquei a ordem dos campos dentro do record.

--- src/main/java/com/example/exampleproject/configs/exceptions/ErrorResponse.java
+++ src/main/java/com/example/exampleproject/configs/exceptions/ErrorResponse.java
@@ -4,14 +4,14 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 @Builder
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public record ErrorResponse(
-        LocalDateTime timestamp,
-        int status,
-        String error,
-        String message,
-        String path
+public record ErrorResponse(LocalDateTime timestamp,
+                            String path,
+                            int status,
+                            String error,
+                            Map<String, String> messages
 ) {
 }
