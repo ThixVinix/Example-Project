637b6a750b2fceb770ebaf21fff8af401dfcbc2a
ThixVinix <tvasouza@hotmail.com>
2024-10-12 18:48:42 -0300

    Reorganizei a estrutura de pacotes movendo MessageUtils para utils.

--- src/main/java/com/example/exampleproject/utils/MessageUtils.java
+++ src/main/java/com/example/exampleproject/utils/MessageUtils.java
@@ -1,4 +1,4 @@
-package com.example.exampleproject.utils.messages;
+package com.example.exampleproject.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
