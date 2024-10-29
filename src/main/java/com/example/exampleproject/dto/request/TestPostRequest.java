e55dd1aca2463a4fd0bbfd2e5367a7d7648e649e
ThixVinix <tvasouza@hotmail.com>
2024-10-29 19:19:31 -0300

    Adicionado a anotação @JsonProperty para melhor compatibilidade com JSON. Renomeado os campos para nomes mais claros e conforme a convenção de nomenclatura em inglês.

--- src/main/java/com/example/exampleproject/dto/request/TestPostRequest.java
+++ src/main/java/com/example/exampleproject/dto/request/TestPostRequest.java
@@ -1,67 +1,82 @@
 package com.example.exampleproject.dto.request;
 
 import com.example.exampleproject.configs.annotations.ValidDateRange;
-import com.example.exampleproject.configs.deserializers.CustomLocalDateDeserializer;
-import com.example.exampleproject.configs.deserializers.CustomLocalDateTimeDeserializer;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.*;
 import org.hibernate.validator.constraints.br.CPF;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @ValidDateRange(dateAField = "dataInicial", dateBField = "dataFinal")
 public record TestPostRequest(
 
-        @JsonDeserialize(using = CustomLocalDateTimeDeserializer.class)
-        LocalDateTime dataInicial,
+        @JsonProperty("dataInicial")
+        LocalDateTime initialDate,
 
-        @JsonDeserialize(using = CustomLocalDateTimeDeserializer.class)
-        LocalDateTime dataFinal,
+        @JsonProperty("dataFinal")
+        LocalDateTime finalDate,
 
         @NotBlank
-        String nome,
+        @JsonProperty("nome")
+        String name,
 
         @NotNull
         @Past
-        @JsonDeserialize(using = CustomLocalDateDeserializer.class)
-        LocalDate dataNascimento,
+        @JsonProperty("dataNascimento")
+        LocalDate dateOfBirth,
 
         @NotBlank
         @Email
+        @JsonProperty("email")
         String email,
 
         @NotBlank
         @CPF
         @Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}",
                 message = "{msg.validation.request.field.cpf.invalidFormat}")
+        @JsonProperty("cpf")
         String cpf,
 
         @NotNull
         @Min(value = 0)
         @Max(value = Byte.MAX_VALUE)
-        Byte idade,
+        @JsonProperty("idade")
+        Byte age,
 
         @NotBlank
-        String imagemBase64,
+        @JsonProperty("imagemBase64")
+        String base64Image,
 
         @NotBlank
         @Pattern(regexp = "\\(\\d{2}\\) \\d{4,5}-\\d{4}",
                 message = "{msg.validation.request.field.phoneNumber.invalidFormat}")
-        String telefone,
+        @JsonProperty("telefone")
+        String telephone,
 
         @Size(min = 3, max = 30)
+        @JsonProperty("username")
         String username,
 
         @Future
         @NotNull
-        @JsonDeserialize(using = CustomLocalDateDeserializer.class)
-        LocalDate dataDeValidade,
+        @JsonProperty("dataDeValidade")
+        LocalDate expirationDate,
 
         @DecimalMin(value = "0.0", inclusive = false)
         @Digits(integer = 5, fraction = 2)
-        BigDecimal preco
+        @JsonProperty("preco")
+        BigDecimal price,
+
+        @NotEmpty
+        @JsonProperty("itens")
+        List<String> items,
+
+        @AssertTrue
+        @JsonProperty("termosConcordados")
+        Boolean isTermsAgreed
 ) {
 }
