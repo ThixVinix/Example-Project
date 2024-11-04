7440d008480bb78d0794a575fa7431151383cb85
ThixVinix <tvasouza@hotmail.com>
2024-11-04 18:17:56 -0300

    Criação do teste unitário da classe principal.

--- src/test/java/com/example/exampleproject/ExampleProjectApplicationTests.java
+++ src/test/java/com/example/exampleproject/ExampleProjectApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.exampleproject;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Tag("ExampleProjectApplication_Tests")
+@DisplayName("ExampleProjectApplication Tests")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ExampleProjectApplicationTests {
 
     @Test
+    @Order(1)
+    @Tag("main_method")
+    @DisplayName("Test main method")
     void contextLoads() {
+        String[] args = {};
+        Assertions.assertDoesNotThrow(() -> ExampleProjectApplication.main(args));
     }
 
 }
