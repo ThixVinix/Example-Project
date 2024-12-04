ca60eef8a55cc86534b7295da1e39e9b7e0fbc8d
ThixVinix <tvasouza@hotmail.com>
2024-12-04 17:01:12 -0300

    feat: adicionado construtor sem argumentos à BusinessException
    O construtor sem argumentos foi adicionado para facilitar a instância da exceção BusinessException em situações onde não é necessário informar uma mensagem detalhada. Isso melhora a flexibilidade e usabilidade da classe ao permitir uma inicialização mais simples.

--- src/main/java/com/example/exampleproject/configs/exceptions/custom/BusinessException.java
+++ src/main/java/com/example/exampleproject/configs/exceptions/custom/BusinessException.java
@@ -1,5 +1,8 @@
 package com.example.exampleproject.configs.exceptions.custom;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
 public class BusinessException extends RuntimeException {
     public BusinessException(String message) {
         super(message);
