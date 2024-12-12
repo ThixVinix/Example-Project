14fb3996b6234645581d62ac10ac51f786fe636a
ThixVinix <tvasouza@hotmail.com>
2024-12-12 01:42:27 -0300

    feat: adicionada configuração de scanBasePackages
    Incluída a configuração de scanBasePackages na anotação @SpringBootApplication. Essa alteração garante a correta detecção de componentes nos pacotes especificados.

--- src/main/java/com/example/exampleproject/ExampleProjectApplication.java
+++ src/main/java/com/example/exampleproject/ExampleProjectApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {"com.example.exampleproject"}
+)
 @EnableFeignClients
 public class ExampleProjectApplication {
 
