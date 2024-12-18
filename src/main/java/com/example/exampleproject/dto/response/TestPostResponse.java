1f36b0748b22ec310f2ca7802acef5069d505994
ThixVinix <tvasouza@hotmail.com>
2024-12-17 21:40:33 -0300

    feat: adicionada documentação com anotações do Swagger
    Anotações do Swagger foram incluídas nos campos de TestPostResponse. Isso melhora a descrição e visibilidade das propriedades na documentação da API. Exemplos e descrições foram configurados para cada campo.

--- src/main/java/com/example/exampleproject/dto/response/TestPostResponse.java
+++ src/main/java/com/example/exampleproject/dto/response/TestPostResponse.java
@@ -1,6 +1,7 @@
 package com.example.exampleproject.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.time.LocalDate;
@@ -10,21 +11,29 @@ import java.time.ZonedDateTime;
 import java.util.Date;
 
 @Builder
+@Schema(description = "Represents the response from a test post endpoint, containing various date and time formats.")
 public record TestPostResponse(
 
         @JsonProperty("data")
+        @Schema(description = "The current system date and time.", example = "2023-11-04T12:45:30.000+00:00")
         Date date,
 
         @JsonProperty("dataLocalHora")
+        @Schema(description = "The current local date and time in 'yyyy-MM-dd'T'HH:mm:ss' format.",
+                example = "2023-11-04T12:45:30")
         LocalDateTime localDateTime,
 
         @JsonProperty("dataLocal")
+        @Schema(description = "The current local date in 'yyyy-MM-dd' format.", example = "2023-11-04")
         LocalDate localDate,
 
         @JsonProperty("ZonaDataHora")
+        @Schema(description = "The current date and time with timezone in ISO-8601 format.",
+                example = "2023-11-04T12:45:30.000Z")
         ZonedDateTime zonedDateTime,
 
         @JsonProperty("horaLocal")
+        @Schema(description = "The current local time in 'HH:mm:ss' format.", example = "12:45:30")
         LocalTime localTime
 ) {
 }
