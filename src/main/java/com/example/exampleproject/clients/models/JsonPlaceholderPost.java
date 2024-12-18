1dccff33db399bc7e0c3166c91fb7d0ff6b24ff0
ThixVinix <tvasouza@hotmail.com>
2024-12-17 21:43:55 -0300

    feat: adicionada documentação Swagger ao modelo JsonPlaceholderPost
    Swagger foi integrado ao modelo JsonPlaceholderPost por meio de anotações @Schema. Isso melhora a documentação da API, fornecendo descrições claras para os campos do modelo.

--- src/main/java/com/example/exampleproject/clients/models/JsonPlaceholderPost.java
+++ src/main/java/com/example/exampleproject/clients/models/JsonPlaceholderPost.java
@@ -1,7 +1,20 @@
 package com.example.exampleproject.clients.models;
 
-public record JsonPlaceholderPost(Long id,
-                                  Long userId,
-                                  String title,
-                                  String body) {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Represents a JsonPlaceholder Post with details like title and body.")
+public record JsonPlaceholderPost(
+        @Schema(description = "Unique identifier of the post.", example = "1")
+        Long id,
+
+        @Schema(description = "ID of the user who created the post.", example = "10")
+        Long userId,
+
+        @Schema(description = "Title of the post.", example = "Introduction to Swagger")
+        String title,
+
+        @Schema(description = "The body/content of the post.",
+                example = "This is an example post about Swagger annotations.")
+        String body
+) {
 }
