bbcbea24a08be1bdc267b6380dada156267c9cde
ThixVinix <tvasouza@hotmail.com>
2024-12-18 17:43:27 -0300

    feat: adicionado suporte ao cabeçalho Accept-Language
    Implementado cabeçalho global Accept-Language nas operações da OpenAPI. Suporta os idiomas: português do Brasil (pt-BR) e inglês (en), permitindo especificar a linguagem preferida para as respostas do serviço.

--- src/main/java/com/example/exampleproject/configs/swagger/OpenAPIConfig.java
+++ src/main/java/com/example/exampleproject/configs/swagger/OpenAPIConfig.java
@@ -2,14 +2,26 @@ package com.example.exampleproject.configs.swagger;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpHeaders;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
 @Profile("!prd")
 @Configuration
 public class OpenAPIConfig {
+
+    private static final String PORTUGUESE_BRAZIL_LANGUAGE_TAG = "pt-BR";
+
+    private static final String ENGLISH_LANGUAGE_TAG = "en";
 
     @Value("${api.title}")
     private String apiTitle;
@@ -27,6 +39,34 @@ public class OpenAPIConfig {
                         .title(apiTitle)
                         .version(apiVersion)
                         .description(apiDescription));
+    }
+
+    @Bean
+    public OpenApiCustomizer globalHeaderCustomizer() {
+        return openApi -> openApi.getPaths().forEach((path, pathItem) ->
+                pathItem.readOperations().forEach(operation -> {
+
+                    final String HEADER_LOCATION = "header";
+
+                    List<Locale> supportedLanguages = List.of(
+                            Locale.forLanguageTag(PORTUGUESE_BRAZIL_LANGUAGE_TAG),
+                            Locale.forLanguageTag(ENGLISH_LANGUAGE_TAG)
+                    );
+
+                    String supportedLanguagesString = supportedLanguages.stream()
+                            .map(Locale::toLanguageTag)
+                            .collect(Collectors.joining(", "));
+
+                    Parameter acceptLanguageHeader = new Parameter()
+                            .in(HEADER_LOCATION)
+                            .name(HttpHeaders.ACCEPT_LANGUAGE)
+                            .description("Preferred language for the service response (Optional). " +
+                                    "Currently supported languages: " + supportedLanguagesString)
+                            .required(Boolean.FALSE)
+                            .schema(new Schema<String>().example(PORTUGUESE_BRAZIL_LANGUAGE_TAG));
+
+                    operation.addParametersItem(acceptLanguageHeader);
+                }));
     }
 
 }
