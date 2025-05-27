03d6212848a90ef8f14f317caef9c76cf3f140fe
ThixVinix <tvasouza@hotmail.com>
2025-05-26 23:25:50 -0300

    feat: adicionada validação de arquivos base64
    Criada anotação "Base64FileValidation" e o validador "Base64FileValidator" para validar strings representando arquivos codificados em base64. Suportados múltiplos tipos MIME, como "application/pdf", validando tanto o formato quanto o conteúdo.

---
+++ src/main/java/com/example/exampleproject/configs/annotations/Base64FileValidation.java
@@ -0,0 +1,50 @@
+package com.example.exampleproject.configs.annotations;
+
+import com.example.exampleproject.configs.annotations.validators.Base64FileValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to validate if a string is a valid base64 encoded file of a specified type.
+ * This annotation checks if the string starts with a valid MIME type prefix
+ * and contains valid base64 encoded data.
+ * <p>
+ * Constraints:
+ * - The string must start with a valid MIME type prefix (e.g., "data:application/pdf;base64,")
+ * - The content after the prefix must be valid base64 encoded data
+ * - Null values are considered valid unless enforced otherwise (e.g., with @NotNull)
+ * - Empty strings are considered valid unless enforced otherwise (e.g., with @NotBlank)
+ */
+@Constraint(validatedBy = Base64FileValidator.class)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Base64FileValidation {
+
+    String message() default "The string is not a valid base64 encoded file.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+    
+    /**
+     * Specifies the allowed MIME types for the base64 encoded file.
+     * Examples: "application/pdf", "image/jpeg", "image/png", etc.
+     * If empty, all MIME types are allowed.
+     * 
+     * @return array of allowed MIME types
+     */
+    String[] allowedTypes() default {
+            "application/pdf",
+            "application/msword",
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document", // .docx
+            "application/vnd.ms-excel",
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", // .xlsx
+            "text/plain",
+            "text/csv"
+    };
+}
