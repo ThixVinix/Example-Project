546fff435378db0971544c3caae02e2d47d63465
ThixVinix <tvasouza@hotmail.com>
2025-06-10 17:31:50 -0300

    refactor: adicionada validação de consistência entre MIME e extensão
    Implementada a validação para verificar a consistência entre o tipo MIME detectado e a extensão do arquivo, com mensagens de erro específicas. Ajustados métodos relacionados para suportar a nova validação, garantindo maior robustez na verificação.

--- src/main/java/com/example/exampleproject/configs/annotations/validators/MultipartFileValidator.java
+++ src/main/java/com/example/exampleproject/configs/annotations/validators/MultipartFileValidator.java
@@ -1,6 +1,7 @@
 package com.example.exampleproject.configs.annotations.validators;
 
 import com.example.exampleproject.configs.annotations.MultipartFileValidation;
+import com.example.exampleproject.configs.annotations.enums.MimeTypeEnum;
 import com.example.exampleproject.utils.MessageUtils;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
@@ -15,7 +16,7 @@ import java.io.InputStream;
 import java.util.Arrays;
 
 /**
- * Validator to check if a MultipartFile is valid according to specified types and size constraints.
+ * Validator to check if a MultipartFile is valid, according to specified types and size constraints.
  */
 @Slf4j
 public class MultipartFileValidator implements ConstraintValidator<MultipartFileValidation, MultipartFile> {
@@ -41,24 +42,32 @@ public class MultipartFileValidator implements ConstraintValidator<MultipartFile
 
     @Override
     public boolean isValid(MultipartFile file, ConstraintValidatorContext context) {
-        // Null or empty files are considered valid (optional validation)
+
         if (file == null || file.isEmpty()) {
             return true;
         }
 
         String detectedType = detectMimeType(file);
+        String originalExtension = getFileExtension(file.getOriginalFilename());
 
-        // Validate the MIME type of the file
-        if (!isMimeTypeAllowed(detectedType)) {
-            String allowedMimeTypes = String.join(", ", allowedTypes);
+        if (!isMimeTypeExtensionConsistent(detectedType, originalExtension)) {
             addConstraintViolation(context,
-                    "msg.validation.request.field.multipartfile.invalid.type",
-                    allowedMimeTypes
-            );
+                    "msg.validation.request.field.multipartfile.invalid.extension",
+                    originalExtension, detectedType);
             return false;
         }
 
-        // Validate the file size against the maximum limit
+        if (!isMimeTypeAllowed(detectedType)) {
+            String allowedMimeTypes = String.join(", ", allowedTypes);
+            addConstraintViolation(context,
+                    "msg.validation.request.field.multipartfile.invalid.type", allowedMimeTypes);
+            return false;
+        }
+
+        return isFileSizeValid(file, context);
+    }
+
+    private boolean isFileSizeValid(MultipartFile file, ConstraintValidatorContext context) {
         final long BYTES_IN_ONE_MB = (1024L * 1024L);
         long maxFileSizeInBytes = maxSizeInMB * BYTES_IN_ONE_MB;
         long actualFileSizeInBytes = file.getSize();
@@ -70,20 +79,18 @@ public class MultipartFileValidator implements ConstraintValidator<MultipartFile
             addConstraintViolation(context,
                     "msg.validation.request.field.multipartfile.invalid.size",
                     String.format("%.4f", actualFileSizeInMB),
-                    String.format("%.0f", maxFileSizeInMB)
-            );
+                    String.format("%.0f", maxFileSizeInMB));
             return false;
         }
 
-        return true; // Valid file
+        return true;
     }
 
     /**
-     * Detects the MIME type of the provided file using an Apache Tika library.
-     * If the real MIME type detection fails, the original content type of the file is returned.
+     * Detects the MIME type of the provided file using Apache Tika.
      *
-     * @param file the {@link MultipartFile} whose MIME type is to be determined
-     * @return the detected MIME type or the original content type if detection fails
+     * @param file the file to detect.
+     * @return the detected MIME type.
      */
     private String detectMimeType(MultipartFile file) {
         try (InputStream is = file.getInputStream()) {
@@ -95,10 +102,42 @@ public class MultipartFileValidator implements ConstraintValidator<MultipartFile
     }
 
     /**
+     * Validates the consistency between MIME type and file extension.
+     *
+     * @param mimeType the detected MIME type.
+     * @param extension the file extension.
+     * @return true if the MIME type matches the extension, false otherwise.
+     */
+    private boolean isMimeTypeExtensionConsistent(String mimeType, String extension) {
+        if (MimeTypeEnum.isNotValidExtension(extension)) {
+            log.warn("No known MIME found for the extension: {}", extension);
+            return false;
+        }
+
+        String expectedExtension = MimeTypeEnum.getExtensionFromMimeType(mimeType);
+
+        return extension.equalsIgnoreCase(expectedExtension);
+    }
+
+
+    /**
+     * Retrieves the file extension from its name.
+     *
+     * @param fileName the file name to extract the extension from.
+     * @return the file extension, or null if it cannot be determined.
+     */
+    private String getFileExtension(String fileName) {
+        if (fileName == null || !fileName.contains(".")) {
+            return null;
+        }
+        return fileName.substring(fileName.lastIndexOf('.') + 1).toLowerCase();
+    }
+
+    /**
      * Checks if the MIME type is in the list of allowed types.
      *
-     * @param contentType the MIME type to check
-     * @return true if the MIME type is allowed, false otherwise
+     * @param contentType the MIME type to check.
+     * @return true if the MIME type is allowed, false otherwise.
      */
     private boolean isMimeTypeAllowed(String contentType) {
         return ArrayUtils.isEmpty(allowedTypes) || Arrays.asList(allowedTypes).contains(contentType);
@@ -107,9 +146,9 @@ public class MultipartFileValidator implements ConstraintValidator<MultipartFile
     /**
      * Adds a custom validation message with parameters.
      *
-     * @param context validation context
-     * @param messageKey the message key to use
-     * @param params parameters to include in the message
+     * @param context validation context.
+     * @param messageKey the message key to use.
+     * @param params the parameters to include in the message.
      */
     private void addConstraintViolation(ConstraintValidatorContext context, String messageKey, String... params) {
         context.disableDefaultConstraintViolation();
@@ -122,3 +161,4 @@ public class MultipartFileValidator implements ConstraintValidator<MultipartFile
                 .addConstraintViolation();
     }
 }
+
