222fd99ec40b9b38c0d191f75426aef7cdcc5596
ThixVinix <tvasouza@hotmail.com>
2025-06-27 00:36:15 -0300

    feat: adicionada validação de tamanho total de arquivos MultipartFile
    Incluída a validação do tamanho total de arquivos em listas de MultipartFile, com o uso de um limite máximo configurado. Essa alteração melhora a validação ao evitar envios que excedam o tamanho permitido, aumentando a robustez do sistema.

--- src/main/java/com/example/exampleproject/configs/annotations/validators/MultipartFileListValidator.java
+++ src/main/java/com/example/exampleproject/configs/annotations/validators/MultipartFileListValidator.java
@@ -5,11 +5,15 @@ import com.example.exampleproject.configs.annotations.validators.base.AbstractLi
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 
 /**
  * Validator to check if a list of MultipartFile objects is valid, according to specified constraints.
@@ -24,10 +28,12 @@ public class MultipartFileListValidator
 
     private MultipartFileValidator multipartFileValidator;
     private int maxFileCount;
+    private int maxTotalSizeMB;
 
     @Override
     public void initialize(MultipartFileValidation annotation) {
         this.maxFileCount = annotation.maxFileCount();
+        this.maxTotalSizeMB = annotation.maxTotalSizeMB();
         multipartFileValidator = new MultipartFileValidator();
         multipartFileValidator.initialize(annotation);
     }
@@ -43,6 +49,11 @@ public class MultipartFileListValidator
             return false;
         }
 
+        if (validateTotalSize(files, maxTotalSizeMB, this::calculateMultipartFileSize, context,
+                "msg.validation.request.field.multipartfile.max.total.size")) {
+            return false;
+        }
+
         if (!validateEachItem(files, this::validateMultipartFile, context, 
                 "msg.validation.request.field.multipartfile.invalid.list")) {
             return false;
@@ -52,24 +63,31 @@ public class MultipartFileListValidator
     }
 
     private boolean validateMultipartFile(MultipartFile file, ConstraintValidatorContext context) {
-        return file == null || multipartFileValidator.isValid(file, context);
+        return isNull(file) || multipartFileValidator.isValid(file, context);
     }
 
     private boolean validateUniqueFileNames(List<MultipartFile> files, ConstraintValidatorContext context) {
         Set<String> uniqueFileNames = new HashSet<>();
 
         for (MultipartFile file : files) {
-            if (file == null) {
+            if (isNull(file)) {
                 continue;
             }
 
             String fileName = file.getOriginalFilename();
-            if (fileName != null && !fileName.isEmpty() && !uniqueFileNames.add(fileName)) {
+            if (nonNull(fileName) && !fileName.isEmpty() && !uniqueFileNames.add(fileName)) {
                 addConstraintViolation(context, "msg.validation.request.field.multipartfile.duplicate.file");
                 return false;
             }
         }
 
         return true;
+    }
+
+    private long calculateMultipartFileSize(MultipartFile file) {
+        if (isNull(file) || file.isEmpty()) {
+            return NumberUtils.INTEGER_ZERO;
+        }
+        return file.getSize();
     }
 }
